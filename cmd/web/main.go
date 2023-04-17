81bff67e2be01ff30fe6339c0e9d488accd0ad4b
Brian Griffin <wbgriffin@pm.me>
2023-04-17 15:53:17 -0400

    feat: Install godotenv

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,16 +1,32 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	router := chi.NewRouter()
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello"))
 	})
 
-	http.ListenAndServe(":8080", router)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	addr := ":" + port
+
+	log.Printf("Starting server on %s", addr)
+	err = http.ListenAndServe(addr, router)
+	log.Fatal(err)
 }
