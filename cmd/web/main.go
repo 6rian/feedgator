6dc986eedcdc3d1566a4328eb0c95a8950fc0db8
Brian Griffin <wbgriffin@pm.me>
2023-04-17 15:47:55 -0400

    feat: Install Chi router

---
+++ cmd/web/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func main() {
+	router := chi.NewRouter()
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	})
+
+	http.ListenAndServe(":8080", router)
+}
