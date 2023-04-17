49c80dda74b73b1e069a033678242ee3753c9926
Brian Griffin <wbgriffin@pm.me>
2023-04-17 17:39:48 -0400

    refactor: pull routes out of main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,9 +5,6 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/chi/middleware"
-	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -17,33 +14,18 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-
-	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:*", "https://localhost:*"},
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	}))
-
-	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello"))
-	})
-
-	apiRouter := chi.NewRouter()
-
-	router.Mount("/v1", apiRouter)
-
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 	addr := ":" + port
 
+	server := http.Server{
+		Addr:    addr,
+		Handler: routes(),
+	}
+
 	log.Printf("Starting server on %s", addr)
-	err = http.ListenAndServe(addr, router)
+	err = server.ListenAndServe()
 	log.Fatal(err)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+)
+
+func routes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:*", "https://localhost:*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello"))
+	})
+
+	apiRouter := chi.NewRouter()
+
+	router.Mount("/v1", apiRouter)
+
+	return router
+}
