b418f8cdcda8e0af95d86bd408b81de958ec280a
Brian Griffin <wbgriffin@pm.me>
2023-04-17 17:32:52 -0400

    feat: Add CORS and logger middleware

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -16,9 +18,24 @@ func main() {
 	}
 
 	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:*", "https://localhost:*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello"))
 	})
+
+	apiRouter := chi.NewRouter()
+
+	router.Mount("/v1", apiRouter)
 
 	port := os.Getenv("PORT")
 	if port == "" {
