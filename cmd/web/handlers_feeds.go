a9ba0f0e9986dc82bdc5a6393fa537c8e9c89c0d
Brian Griffin <wbgriffin@pm.me>
2023-04-19 18:24:21 -0400

    feat: automatically follow feed upon creation

--- cmd/web/handlers_feeds.go
+++ cmd/web/handlers_feeds.go
@@ -43,7 +43,32 @@ func (app *application) handleCreateFeed(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	respondWithJSON(w, http.StatusCreated, feed)
+	// Automatically follow the feed
+	ffParams := &database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		UserID:    user.ID,
+		FeedID:    feed.ID,
+	}
+	feedFollow, err := app.DB.CreateFeedFollow(r.Context(), *ffParams)
+	if err != nil {
+		// TODO: add logging
+		respondWithError(w, http.StatusInternalServerError, "could not follow feed")
+		return
+	}
+
+	type response struct {
+		Feed       database.Feed       `json:"feed"`
+		FeedFollow database.FeedFollow `json:"feed_follow"`
+	}
+
+	data := &response{
+		Feed:       feed,
+		FeedFollow: feedFollow,
+	}
+
+	respondWithJSON(w, http.StatusCreated, data)
 }
 
 func (app *application) handleGetFeeds(w http.ResponseWriter, r *http.Request) {
